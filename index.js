7bb58b7b6409d3cda0a3ef79fa45c25f30be3076
CO_DE_PILOT <piratosnogi@gmail.com>
2024-01-03 11:06:03 +0100

    Scroll-watcher, contact form is working

--- index.js
+++ index.js
@@ -13,3 +13,64 @@ menuToggle.addEventListener('click', () => {
     }
 })
 
+
+/* CONTACT FORM vezérlése */
+const submitButton = document.querySelector('.submit-button');
+const email = document.querySelector('.field #email');
+const form = document.querySelector('.contact-form');
+const items = document.querySelectorAll('.form-item');
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    checkInputs();
+    let correctFormInput = true;
+    for (const item of items) {
+        if (item.classList.contains("blank") || item.classList.contains("error")) {
+            correctFormInput = false;
+        }
+    }
+    if (correctFormInput) {
+        form.submit();
+        form.reset();
+    }
+})
+
+function checkInputs() {
+    
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("blank");
+            item.parentElement.classList.add("blank");
+        }
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener('keyup', (event) => {
+            checkEmail();
+        });
+
+        
+        item.addEventListener('keyup', (event) => {            
+            if (item.value != "") {
+                item.classList.remove("blank");
+                item.parentElement.classList.remove("blank");
+            } else {
+                item.classList.add("blank");
+                item.parentElement.classList.add("blank");
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+    } else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
