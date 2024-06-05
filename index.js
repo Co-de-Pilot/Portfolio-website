ab9c7e9c313b7cfa878bb01caad1307528cac844
CO_DE_PILOT <piratosnogi@gmail.com>
2024-06-05 20:56:28 +0200

    CSS nesting & HTML, JS refactoring

--- index.js
+++ index.js
@@ -1,136 +1,131 @@
 /* Nyelv kiválasztás vezérlése */
-const hunButton = document.querySelector('.lang-hun');
-const hunItems = document.querySelectorAll('.hun');
-const engButton = document.querySelector('.lang-eng');
-const engItems = document.querySelectorAll('.eng');
+const hunButton = document.querySelector(".lang-hun");
+const hunItems = document.querySelectorAll(".hun");
+const engButton = document.querySelector(".lang-eng");
+const engItems = document.querySelectorAll(".eng");
 
-hunButton.addEventListener('click', () => {
-    hunItems.forEach(hunItem => {
-        hunItem.classList.remove('lang-inactive');
-    });
-    engItems.forEach(engItem => {
-        engItem.classList.add('lang-inactive');
-    });
-})
+hunButton.addEventListener("click", () => {
+  hunItems.forEach((hunItem) => {
+    hunItem.classList.remove("lang-inactive");
+  });
+  engItems.forEach((engItem) => {
+    engItem.classList.add("lang-inactive");
+  });
+});
 
-engButton.addEventListener('click', () => {
-    engItems.forEach(engItem => {
-        engItem.classList.remove('lang-inactive');
-    });
-    hunItems.forEach(hunItem => {
-        hunItem.classList.add('lang-inactive');
-    });
-})
-
+engButton.addEventListener("click", () => {
+  engItems.forEach((engItem) => {
+    engItem.classList.remove("lang-inactive");
+  });
+  hunItems.forEach((hunItem) => {
+    hunItem.classList.add("lang-inactive");
+  });
+});
 
 /* LIGHT/DARK MODE vezérlése */
-const lightModeButton = document.querySelector('.light-mode');
-const darkModeButton = document.querySelector('.dark-mode');
+const lightModeButton = document.querySelector(".light-mode");
+const darkModeButton = document.querySelector(".dark-mode");
 
-lightModeButton.addEventListener('click', () => {
-    document.body.classList.remove("dark-bg");
-})
+lightModeButton.addEventListener("click", () => {
+  document.body.classList.remove("dark-bg");
+});
 
-darkModeButton.addEventListener('click', () => {
-    document.body.classList.add("dark-bg");
-})
-
+darkModeButton.addEventListener("click", () => {
+  document.body.classList.add("dark-bg");
+});
 
 /* MENU TOGGLE vezérlése */
-const menuToggle = document.querySelector('nav .toggle');
-const menuList = document.querySelector('nav .menu');
+const menuToggle = document.querySelector("nav .toggle");
+const menuList = document.querySelector("nav .menu");
 
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle("menu-active");
-    menuList.classList.toggle("menu-active");
-})
-
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("menu-active");
+  menuList.classList.toggle("menu-active");
+});
 
 /* MENU ACTIVE LINK vezérlése */
-const sections = document.querySelectorAll('section');
-const menuitems = document.querySelectorAll('nav .menu li a');
+const sections = document.querySelectorAll("section");
+const menuitems = document.querySelectorAll("nav .menu li a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            menuitems.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('nav .menu li a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
+    if (top >= offset && top < offset + height) {
+      menuitems.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("nav .menu li a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 };
 
-/* id="name" */
-
-
 /* CONTACT FORM vezérlése */
-const form = document.querySelector('.contact-form');
-const submitButtons = document.querySelectorAll('.submit-button');
+const form = document.querySelector(".contact-form");
+const submitButtons = document.querySelectorAll(".submit-button");
 let items;
 
-for(const submitButton of submitButtons) {
-    submitButton.addEventListener('click', (event) => {
-        items = document.querySelectorAll('.form-item:not(.lang-inactive)');
-        console.log(items);
-        event.preventDefault();
-        checkInputs();
-        let correctFormInput = true;
-        for (const item of items) {
-            if (item.classList.contains("blank") || item.classList.contains("error")) {
-                correctFormInput = false;
-            }
-        }
-        if (correctFormInput) {
-            form.submit();
-            form.reset();
-        }
-    })
+for (const submitButton of submitButtons) {
+  submitButton.addEventListener("click", (event) => {
+    items = document.querySelectorAll(".form-item:not(.lang-inactive)");
+    event.preventDefault();
+    checkInputs();
+    let correctFormInput = true;
+    for (const item of items) {
+      if (
+        item.classList.contains("blank") ||
+        item.classList.contains("error")
+      ) {
+        correctFormInput = false;
+      }
+    }
+    if (correctFormInput) {
+      form.submit();
+      form.reset();
+    }
+  });
 }
 
 function checkInputs() {
-    console.log(items);
-
-    for (const item of items) {
-        if (item.value == "") {
-            item.classList.add("blank");
-            item.parentElement.classList.add("blank");
-        }
-        if (items[1].value != "") {
-            checkEmail();
-        }
-        
-        items[1].addEventListener('keyup', (event) => {
-            checkEmail();
-        });
-        
-        
-        item.addEventListener('keyup', (event) => {            
-            if (item.value != "") {
-                item.classList.remove("blank");
-                item.parentElement.classList.remove("blank");
-            } else {
-                item.classList.add("blank");
-                item.parentElement.classList.add("blank");
-            }
-        });
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("blank");
+      item.parentElement.classList.add("blank");
     }
+    if (items[1].value != "") {
+      checkEmail();
+    }
+
+    items[1].addEventListener("keyup", (event) => {
+      checkEmail();
+    });
+
+    item.addEventListener("keyup", (event) => {
+      if (item.value != "") {
+        item.classList.remove("blank");
+        item.parentElement.classList.remove("blank");
+      } else {
+        item.classList.add("blank");
+        item.parentElement.classList.add("blank");
+      }
+    });
+  }
 }
 
 function checkEmail() {
-    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
-    let email = document.querySelector('.email:not(.lang-inactive)');
-    
-    if (!email.value.match(emailRegex)) {
-        email.classList.add("error");
-        email.parentElement.classList.add("error");
-    } else {
-        email.classList.remove("error");
-        email.parentElement.classList.remove("error");
-    }
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+  let email = document.querySelector(".email:not(.lang-inactive)");
+
+  if (!email.value.match(emailRegex)) {
+    email.classList.add("error");
+    email.parentElement.classList.add("error");
+  } else {
+    email.classList.remove("error");
+    email.parentElement.classList.remove("error");
+  }
 }
